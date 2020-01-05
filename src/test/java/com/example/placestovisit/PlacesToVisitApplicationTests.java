8da27fcfdd914b04a95e6b28b110999480d1bbe4
Andrea Lata <andrelata12@gmail.com>
2020-01-05 20:25:28 -0300

    start project

---
+++ src/test/java/com/example/placestovisit/PlacesToVisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placestovisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesToVisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
